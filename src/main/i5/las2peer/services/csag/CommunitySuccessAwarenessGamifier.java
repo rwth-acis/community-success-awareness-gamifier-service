32d254cc0dad7a68a0cf1acf45b180770f680613
Martin <martin.hug@rwth-aachen.de>
2020-01-20 12:03:58 +0100

    Initial las2peer service project

---
+++ src/main/i5/las2peer/services/csag/CommunitySuccessAwarenessGamifier.java
@@ -0,0 +1,90 @@
+package i5.las2peer.services.csag;
+
+import java.net.HttpURLConnection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import i5.las2peer.api.Context;
+import i5.las2peer.api.security.UserAgent;
+import i5.las2peer.restMapper.RESTService;
+import i5.las2peer.restMapper.annotations.ServicePath;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SwaggerDefinition;
+
+/**
+ * Community Success Awareness Gamifier
+ * 
+ */
+
+@Api
+@SwaggerDefinition(
+		info = @Info(
+				title = "Community Success Awareness Gamifier",
+				version = "1.0.0",
+				description = "Community Success Awareness Gamifier Service",
+				termsOfService = "http://your-terms-of-service-url.com",
+				contact = @Contact(
+						name = "Martin Hug",
+						email = "hug@dbis.rwth-aachen.de"),
+				license = @License(
+						name = "MIT",
+						url = "https://opensource.org/licenses/MIT")))
+@ServicePath("/csag")
+public class CommunitySuccessAwarenessGamifier extends RESTService {
+
+	/**
+	 * Template of a get function.
+	 * 
+	 * @return Returns an HTTP response with the username as string content.
+	 */
+	@GET
+	@Path("/get")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "REPLACE THIS WITH YOUR NOTES TO THE FUNCTION")
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	public Response getTemplate() {
+		//UserAgent userAgent = (UserAgent) Context.getCurrent().getMainAgent();
+		String response = "ok";
+		return Response.ok().entity(response).build();
+	}
+
+	/**
+	 * Template of a post function.
+	 * 
+	 * @param myInput The post input the user will provide.
+	 * @return Returns an HTTP response with plain text string content derived from the path input param.
+	 */
+	@POST
+	@Path("/post/{input}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "Example method that returns a phrase containing the received input.")
+	public Response postTemplate(@PathParam("input") String myInput) {
+		String returnString = "";
+		returnString += "Input " + myInput;
+		return Response.ok().entity(returnString).build();
+	}
+
+}
